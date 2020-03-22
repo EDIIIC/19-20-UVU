41079eb753e0289f240c8c5300e0625063db64a5
Ethan Cragun <craguned@gmail.com>
2020-03-21 19:01:39 -0600

    Level Navmesh and Lighitng work

---
+++ Level Design/Wend Unity/Assets/Scripts/Enemy.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class Enemy : MonoBehaviour
+{
+    private NavMeshAgent Mob;
+    public GameObject Player;
+    public float MobDistanceRun = 4.0f;
+
+    void Start()
+    {
+        Mob = GetComponent<NavMeshAgent>();
+    }
+
+    void Update()
+    {
+        float distance = Vector3.Distance(transform.position, Player.transform.position);
+        //run towards player
+        if (distance < MobDistanceRun)
+        {
+            Vector3 dirToPlayer = transform.position - Player.transform.position;
+            Vector3 newPos = transform.position - dirToPlayer;
+
+            Mob.SetDestination(newPos);
+        }
+
+    }
+}
